8a3db3ce14548c0779d7b761c415c39595e39415
EVANDRO-BATISTA <64389438+EVANDRO-BATISTA@users.noreply.github.com>
2025-08-19 21:51:55 +0000

    testando crud

--- app.py
+++ app.py
@@ -2,11 +2,22 @@ import os
 from flask import Flask
 from config import Config
 from controllers.user_controller import UserController
+from models.user import db
 
 app = Flask(__name__, template_folder=os.path.join('view', 'templates'))
 app.config.from_object(Config)
 
+# Inicializa o banco de dados
+db.init_app(app)
+
+# Criar as tabelas
+with app.app_context():
+    db.create_all()
+
+#Rotas
 app.add_url_rule('/', 'index', UserController.index)
+app.add_url_rule('/contact', 'contact', UserController.contact, methods=['GET', 'POST'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,23 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from models.user import User, db
 
 class UserController:
     @staticmethod
     def index():
-        return render_template('index.html')
+        users = User.query.all()
+        return render_template('index.html', users=users)
+
+    @staticmethod
+    def contact():
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+
+            new_user = User(name=name, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
+
+        return render_template('contact.html')
